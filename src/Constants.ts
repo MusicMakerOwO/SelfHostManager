56e93e6be93d8b189f44bb8ee37e4e4bce558cbb
Music Maker <joshuanejdl@gmail.com>
2025-01-09 12:27:55 -0600

    feat: Global constants

---
+++ src/Constants.ts
@@ -0,0 +1,4 @@
+export const ROOT_FOLDER	 = `${__dirname}/..`;
+export const LOGS_FOLDER	 = `${ROOT_FOLDER}/Logs`;
+export const BOT_FOLDER 	 = `${ROOT_FOLDER}/Bots`;
+export const COMMANDS_FOLDER = `${ROOT_FOLDER}/Commands`;
