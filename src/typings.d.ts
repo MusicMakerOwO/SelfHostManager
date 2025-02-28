4af928742a606e3a668d86ab8ea1f9f4210df3d8
Music Maker <joshuanejdl@gmail.com>
2025-02-28 08:44:45 -0600

    fix: Updating typings in preparation for commands

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,11 +1,6 @@
-import { ChildProcess } from "child_process";
-
 export type CommandFile = {
 	name: string;
 	description: string;
 	usage: string;
-	execute: () => void;
+	execute: Function;
 };
-
-export type EventCallback = (process: ChildProcess, bot: string, ...args: any[]) => void;
-export type EventLookup = Record<string, EventCallback>;
