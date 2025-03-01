cd5a9cf5571f46581286ab7ac9b05f754f5a0163
Music Maker <joshuanejdl@gmail.com>
2025-03-01 15:10:53 -0600

    feat: Added help section for specific commands

--- src/Commands/help.ts
+++ src/Commands/help.ts
@@ -3,8 +3,22 @@ import { CommandFile } from "../typings";
 export default {
 	name: 'help',
 	description: 'Get a list of all commands',
-	usage: 'help',
-	execute (commands: Map<string, CommandFile>) {
+	usage: 'help [command]',
+	execute (commands: Map<string, CommandFile>, args: string[]) {
+
+		if (args.length > 0) {
+			const command = commands.get(args[0]);
+			if (!command) {
+				console.log('Command not found');
+				return;
+			}
+
+			console.log(`Name: ${command.name}`);
+			console.log(`Description: ${command.description}`)
+			console.log(`Usage: ${command.usage}`);
+			return;
+		}
+
 		let output = '';
 
 		const longestCommandName = Math.max(...[...commands.keys()].map(name => name.length));
